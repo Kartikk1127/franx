32a56f98144e54967602da6f93e0f3a4df807bfd
Kartikey Srivastava <kartikey.srivastava1271@gmail.com>
2025-11-19 00:07:18 +0530

    first commit

---
+++ src/main/java/com/franx/FranxApplication.java
@@ -0,0 +1,12 @@
+package com.franx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranxApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FranxApplication.class, args);
+	}
+
+}
